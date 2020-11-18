a4cc2740d1ccdb238a366ad7d1726571c918004b
Yu410902038 <71751927+Yu410902038@users.noreply.github.com>
2020-11-18 15:32:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A win!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B win!")
+})
+basic.forever(function () {
+	
+})
